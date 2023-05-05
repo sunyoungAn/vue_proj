bb6dbf5d174b595f3ee36bcdf87c54cfc414279e
sunyoungAn <120145637+sunyoungAn@users.noreply.github.com>
2023-05-05 22:01:38 +0900

    url path 오타수정
    url path 오타수정

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -23,7 +23,7 @@
                     </ul>
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="/admin/prdouct/list">관리자메뉴</a>
+                            <a class="nav-link" href="/admin/product/list">관리자메뉴</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">고객센터</a>
