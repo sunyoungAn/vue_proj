71317339a638c6baddac8c5fe97414c3049f179d
sunyoungAn <120145637+sunyoungAn@users.noreply.github.com>
2023-05-11 22:42:19 +0900

    결제관리 기능 구현중
    결제관리 기능 구현중

--- src/components/AdminPaymentDeliveryModal.vue
+++ src/components/AdminPaymentDeliveryModal.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="admin_blackBg"> <!--v-if="openmodal"-->
+    <div v-if="state.openmodal" class="admin_blackBg">
         <div class="admin_whiteBg_payment_delivery">
             <div class="modal-header">
                 <h3 class="modal-title">결제/배송상세정보</h3>
-                <button type="button" class="btn-close" aria-label="Close"></button>
+                <button type="button" class="btn-close" @click="closeModal()"></button>
             </div>
             <div class="modal-body">
                 <hr>
@@ -24,56 +24,82 @@
                 <!-- 배송정보 -->
                 <div class="mb-3 ms-3">
                     <h4>| 받는사람</h4>
-                    <div class="ps-5 py-2 modal_input">
-                        <input type="text" class="form-control">
-                    </div>
+                    <h5 class="ps-5 py-2">2023년 4월 22일</h5>
                 </div>
                 <div class="mb-3 ms-3">
                     <h4>| 휴대폰번호</h4>
-                    <div class="ps-5 py-2 modal_input">
-                        <input type="text" class="form-control">
-                    </div>
+                    <h5 class="ps-5 py-2">2023년 4월 22일</h5>
                 </div>
                 <div class="mb-3 ms-3">
                     <h4>| 배송주소</h4>
-                    <div class="ps-5 py-2 modal_input_address row">
-                        <div class="col-2">
-                            <input type="text" class="form-control">
-                        </div>
-                        <div class="col-10">
-                            <input type="text" class="form-control">
-                        </div>
-                    </div>
+                    <h5 class="ps-5 py-2">2023년 4월 22일</h5>
                 </div>
                 <div class="mb-3 ms-3">
                     <h4>| 배송메세지</h4>
-                    <div class="ps-5 py-2 modal_input_long">
-                        <input type="text" class="form-control">
-                    </div>
+                    <h5 class="ps-5 py-2">2023년 4월 22일</h5>
                 </div>
 
                 
             </div>
-            <!-- <div class="d-flex justify-content-center"> -->
             <div class="modal-footer">   
-                <button type="button" class="btn btn-success btn-lg me-3">배송수정</button>
-                <button type="button" class="btn btn-success btn-lg">Close</button>
+                <button type="button" class="btn btn-success btn-lg" @click="closeModal()">Close</button>
             </div>
-            <!-- <img :src=products[index].imageurl />
-            <h4>{{ products[index].p_name }}</h4>
-            <P>{{ products[index].content }}</P>
-            <P>{{ products[index].price }}원</P> -->
-            <!-- <button @click="modalClose">닫기</button> -->
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    setup () {
-        
+import { onMounted, reactive } from 'vue'; 
+// import axios from 'axios';
 
-        return {}
+export default {
+
+    props : {
+        openmodal : {
+            type : Boolean
+        },
+        id : {
+            type : Number
+        }
+    },
+
+    setup ( props, context ) {
+        
+        const state = reactive({
+            id : props.id,
+            openmodal : props.openmodal,
+            paymentType : '',
+            price : 0,
+            paymentDate : ''
+        })
+
+        const closeModal = () => {
+            context.emit('closemodal', false);
+        }
+
+        const loadData = () => {
+
+            // axios.get(`/api/admin/inventory/getpayment/${state.id}`).then((res)=>{
+            //     console.log(res.data);
+            //     state.paymentType = res.data.paymentType;
+            //     state.price = changePriceFormat(res.data.price);
+            //     state.paymentDate = changeDateFormat(res.data.registDate);
+            // }).catch(()=>{
+            // })
+
+        }
+
+
+        // 모달창 열릴 때 불러와질예정
+        onMounted(() => {
+            loadData();
+        })
+
+
+        return {
+            state,
+            closeModal
+        }
     }
 }
 </script>
--- src/views/AdminContractListView.vue
+++ src/views/AdminContractListView.vue
@@ -11,16 +11,15 @@
                     <div class="row mb-3">
                         <div class="col-2 py-2">
                             거래번호
-                            <!-- <p class="text-center">번호</p> -->
                         </div>
                         <div class="col-4">
-                            <input type="text" class="form-control" placeholder="거래번호">
+                            <input type="text" class="form-control" v-model="state.searchId" placeholder="거래번호">
                         </div>
                         <div class="col-2 py-2">
-                            상품번호 <!-- TODO 구매입찰, 판매입찰 관련해서 아이디를 이름등으로 바꾸는게 좋을련가?-->
+                            상품번호
                         </div>
                         <div class="col-4">
-                            <input type="text" class="form-control" placeholder="상품번호">
+                            <input type="text" class="form-control" v-model="state.searchProductId" placeholder="상품번호">
                         </div>
                     </div>
                     <div class="row mb-3">
@@ -28,13 +27,13 @@
                             판매자번호
                         </div>
                         <div class="col-4">
-                            <input type="text" class="form-control" placeholder="판매자번호">
+                            <input type="text" class="form-control" v-model="state.searchSellerMemberNumber" placeholder="판매자번호">
                         </div>
                         <div class="col-2 py-2">
                             구매자번호
                         </div>
                         <div class="col-4">
-                            <input type="text" class="form-control" placeholder="구매자번호">
+                            <input type="text" class="form-control" v-model="state.searchBuyerMemberNumber" placeholder="구매자번호">
                         </div>
                     </div>
                     <div class="row mb-3">
@@ -42,20 +41,16 @@
                             판매상태
                         </div>
                         <div class="col-4">
-                            <select class="form-select brand_select" id="inputGroupSelect01">
-                                <option selected>Choose...</option>
-                                <option value="1">판매상태1</option>
-                                <option value="2">판매상태2</option>
+                            <select class="form-select brand_select" v-model="state.searchSellingStatus">
+                                <option v-for="status in state.sellingStatusList" :key="status" :value="status.id">{{ status.value }}</option>
                             </select>
                         </div>
                         <div class="col-2 py-2">
                             구매상태
                         </div>
                         <div class="col-4">
-                            <select class="form-select brand_select" id="inputGroupSelect01">
-                                <option selected>Choose...</option>
-                                <option value="1">구매상태1</option>
-                                <option value="2">구매상태2</option>
+                            <select class="form-select brand_select" v-model="state.searchBuyingStatus">
+                                <option v-for="status in state.buyingStatusList" :key="status" :value="status.id">{{ status.value }}</option>
                             </select>
                         </div>
                     </div>
@@ -64,112 +59,124 @@
                             거래체결일
                         </div>
                         <div class="col-4">
-                            <input type="date" class="form-control">
+                            <input type="date" class="form-control" v-model="state.searchContractDateStart">
                         </div>
                         <div class="col-2 py-2">
                             <p class="text-center">~</p>
                         </div>
                         <div class="col-4">
-                            <input type="date" class="form-control">
+                            <input type="date" class="form-control" v-model="state.searchContractDateEnd">
                         </div>
                     </div>
                     
                     
                 </div>
                 <div class="d-flex justify-content-center mt-4">
-                    <button class="btn btn-success btn-lg">검색</button>
+                    <button class="btn btn-success btn-lg" @click="search()">검색</button>
                 </div>
             </div>
 
             <!-- 검색 결과-->
             <div>
-                <table class="table">
+                <table class="table  align-middle">
                     <thead>
-                        <tr>
-                            <th scope="col">
-                                <input class="form-check-input" type="checkbox" value="">
+                        <tr class="text-center">
+                            <th scope="col" width="50px">
+                                <input class="form-check-input" type="checkbox" :checked="state.checkAll" @change="checkAllEvnet()">
                             </th>
                             <th scope="col">거래번호</th>
                             <th scope="col">상품번호</th>
-                            <th scope="col">사이즈</th>
+                            <!-- <th scope="col">사이즈</th> -->
                             <th scope="col">판매자번호</th>
                             <th scope="col">구매자번호</th>
-                            <th scope="col">거래체결일</th>
-                            <th scope="col">판매상태</th>
-                            <th scope="col">구매상태</th>
-                            <th scope="col">정산일</th>
+                            <th scope="col" width="107px">거래체결일</th>
+                            <th scope="col" width="150px">판매상태</th>
+                            <th scope="col" width="150px">구매상태</th>
+                            <th scope="col" width="107px">정산일</th>
                             <th scope="col">결제&배송정보</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>11</td> <!-- TODO 상품상세페이지로 이동하게 할까?-->
-                            <td>250</td>
-                            <td>55</td>
-                            <td>666</td>
-                            <td>2023/04/07</td>
+                        <tr class="text-center" v-for="data of state.rows" :key="data">
+                            <td><input class="form-check-input" type="checkbox" :value="data.id" v-model="state.checkList" @change="checkEvnet()"></td>
+                            <td>{{ data.id }}</td>
                             <td>
-                                <select class="form-select brand_select" id="inputGroupSelect01">
-                                    <option selected>등록중</option>
-                                    <option value="1">등록중</option>
-                                    <option value="2">등록중</option>
-                                </select>
+                                <el-popover placement="top-start" :title=data.productEngName :width="400" trigger="hover" :content=data.productKorName+state.sizeLabel+data.productSize>
+                                    <template #reference>
+                                        {{ data.productId }}
+                                    </template> 
+                                </el-popover>
+                            </td>
+                            <!-- <td>250</td> -->
+                            <td v-if="data.sellerNumber != null">
+                                <el-popover placement="top-start" :title=data.sellerName :width="300" trigger="hover" :content=data.sellerEmail>
+                                    <template #reference>
+                                        {{ data.sellerNumber }}
+                                    </template> 
+                                </el-popover>
+                            </td>
+                            <td v-else>
+                                -
                             </td>
                             <td>
-                                <select class="form-select brand_select" id="inputGroupSelect01">
-                                    <option selected>등록중</option>
-                                    <option value="1">등록중</option>
-                                    <option value="2">등록중</option>
+                                <el-popover placement="top-start" :title=data.buyerName :width="300" trigger="hover" :content=data.buyerEmail>
+                                    <template #reference>
+                                        {{ data.buyerNumber }}
+                                    </template> 
+                                </el-popover>
+                            </td>
+                            <td>{{ changeDateFormat(data.contractDate) }}</td>
+                            <td v-if="data.sellingStatus !== null">
+                                <select class="form-select" v-model="data.sellingStatus">
+                                    <option v-for="status in state.sellingStatusList" :key="status" :value="status.id">{{ status.value }}</option>
                                 </select>
                             </td>
-                            <td>2023/04/07</td>
+                            <td v-else>
+                                브랜드배송
+                            </td>
                             <td>
-                                <button type="button" class="btn btn-outline-success" @click="openModal()">상세보기</button>
+                                <select class="form-select" v-model="data.buyingStatus">
+                                    <option v-for="status in state.buyingStatusList" :key="status" :value="status.id">{{ status.value }}</option>
+                                </select>
+                            </td>
+                            <td v-if="data.paidDate !== null">
+                                <el-popover placement="top-start" title="정산금액" :width="150" trigger="hover" :content=changePriceFormat(data.paidPrice)>
+                                    <template #reference>
+                                        {{ changeDateFormat(data.paidDate) }}
+                                    </template> 
+                                </el-popover>
+                            </td>
+                            <td v-else>-</td>
+                            <td>
+                                <button type="button" class="btn btn-outline-success btn-sm" @click="state.isModalShow=true; state.modalTargetId=data.id;">상세보기</button>
                             </td>
                         </tr>
-                        
                     </tbody>
                 </table>
             </div>
             
             <!-- 페이지 네이션-->
             <nav class="d-flex justify-content-center mt-4">
-                <ul class="pagination">
-                    <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                    </li>
-                </ul>
+                <el-pagination background layout="prev, pager, next" @current-change="changePage" :total="state.total" />
             </nav>
 
-            <div class="d-flex justify-content-center mt-3">
-                <button class="btn btn-success btn-lg d-flex justify-content-start">상태변경</button>
+            <div class="d-flex justify-content-center mt-5">
+                <button class="btn btn-success btn-lg d-flex justify-content-start" @click="editStatus()">상태변경</button>
             </div> 
         </div>
 
     </div>
 
     <!-- 모달영역 -->
-    <admin-payment-delivery-modal v-if="state.isModalShow"></admin-payment-delivery-modal>
-
+    <admin-payment-delivery-modal v-if="state.isModalShow" :openmodal="state.isModalShow" :id="state.modalTargetId" @closemodal="modalClose"></admin-payment-delivery-modal>
 
 </template>
 
 <script>
 import AdminSubMenu from '@/components/AdminSubMenu.vue'
 import AdminPaymentDeliveryModal from '@/components/AdminPaymentDeliveryModal.vue'
-import { reactive } from 'vue'
+import { onMounted, reactive } from 'vue'; 
+import axios from 'axios';
 
 export default {
     name: 'SubMenu',
@@ -180,23 +187,175 @@ export default {
     setup () {
 
         const state = reactive({
-            isModalShow:false
+            isModalShow : false, // 모달창 제어 변수
+            modalTargetId : 0,
+            rows : [],
+            total : 0,
+            page : 0,
+            isSearch : false,
+            checkAll : false,
+            checkList : [],
+            sellingStatusList : [
+                { id : 0, value : '판매상태선택'}, 
+                { id : 21, value : '발송요청'}, // 21~27 진행중
+                { id : 22, value : '발송완료'},
+                { id : 23, value : '입고대기'},
+                { id : 24, value : '입고완료'},
+                { id : 25, value : '검수중'},
+                { id : 26, value : '검수불합격'},
+                { id : 27, value : '검수합격'},
+                { id : 50, value : '정산완료'}, // 50~53 종료
+                { id : 51, value : '취소완료'},
+                { id : 52, value : '불합격반송'},
+                { id : 53, value : '회수완료'},
+            ],
+            inventoryStatusList : [
+                { id : 0, value : '판매상태선택'}, 
+                { id : 50, value : '정산완료'}, // 50~53 종료
+                { id : 51, value : '취소완료'},
+                { id : 52, value : '불합격반송'},
+                { id : 53, value : '회수완료'},
+            ],
+            buyingStatusList : [
+                { id : 0, value : '구매상태선택'},
+                { id : 31, value : '대기중'}, // 31~45 진행중
+                { id : 32, value : '발송완료'},
+                { id : 33, value : '입고대기'},
+                { id : 34, value : '입고완료'},
+                { id : 35, value : '검수중'},
+                { id : 36, value : '거래실패'},
+                { id : 37, value : '검수합격'},
+                { id : 38, value : '배송중'}, // 브랜드, 거래 모두 포함
+                { id : 39, value : '상품준비중'}, //39~45 브랜드
+                { id : 40, value : '반품신청'},
+                { id : 41, value : '접수완료'},
+                { id : 42, value : '회수중'},
+                { id : 43, value : '회수완료'},
+                { id : 44, value : '교환신청'},
+                { id : 45, value : '교환중'},
+                { id : 60, value : '배송완료'}, // 60~63 종료
+                { id : 61, value : '취소완료'},
+                { id : 62, value : '반품완료'},
+                { id : 63, value : '교환완료'},
+            ],
+            searchId : '', // 검색조건
+            searchProductId : '', // 검색조건
+            searchSellerMemberNumber : '', // 검색조건
+            searchBuyerMemberNumber : '', // 검색조건
+            searchSellingStatus : 0, // 검색조건
+            searchBuyingStatus : 0, // 검색조건
+            searchContractDateStart : '', // 검색조건
+            searchContractDateEnd : '', // 검색조건
+            originalDatas : [],
+            sizeLabel : ' - size : ' 
         })
         
+        // 보관상품관리리스트페이지에 띄울 정보 모두 가져오기
+        const loadData = () => {
+            axios.get(`/api/admin/contract/getall?page=${state.page}`).then((res)=>{
+                console.log(res.data);
+                state.rows = res.data.contractList;
+                state.total = res.data.total;
+                state.checkAll = false;
+                state.checkList = [];
+                state.originalDatas = res.data.contractList;
+                // 검색조건 초기화
+                state.searchId = '';
+                state.searchProductId = '';
+                state.searchSellerMemberNumber = '';
+                state.searchBuyerMemberNumber = '';
+                state.searchSellingStatus = 0;
+                state.searchBuyingStatus = 0;
+                state.searchContractDateStart = '';
+                state.searchContractDateEnd = '';
+            }).catch(()=>{
+            })
+        }
 
-        const openModal = () => {
-
-            if(state.isModalShow === true) {
-                state.isModalShow = false;
+        // 페이징기능
+        const changePage = (page) => {
+            console.log(page);
+            state.page = page - 1; // 상태변수값 변경
+            if(state.isSearch === false) {
+                loadData(); // 데이터가져오기
             } else {
-                state.isModalShow = true;
+                search();
             }
+        }
+
+        // 검색하기
+        const search = () => {
 
         }
 
+        // 전체선택 처리
+        const checkAllEvnet = () => {
+            if(state.checkAll === false) {
+                for(let idx in state.rows) {
+
+                    // 이미 선택되어 있는 경우에는 중복선택 제외하기
+                    if(state.checkList.includes(state.rows[idx].id)) {
+                        continue;
+                    }
+                    state.checkList.push(state.rows[idx].id);
+                }
+                state.checkAll = true;
+            } else {
+                state.checkList = [];
+                state.checkAll = false;
+            }
+        }
+        
+        // 체크박스 개별 처리
+        const checkEvnet = () => {
+            if(state.checkList.length === state.rows.length) {
+                state.checkAll = true;
+            } else {
+                state.checkAll = false;
+            }
+        }
+
+        // 날짜형식변환 yyyy/mm/dd
+        const changeDateFormat = (data) => {
+            let date = new Date(data);
+            let year = date.getFullYear();
+            let month = ("0" + (1 + date.getMonth())).slice(-2);
+            let day = ("0" + date.getDate()).slice(-2);
+
+            return year + "/" + month + "/" + day;
+        }
+
+        // 금액형식변환 세자리마다 콤마추가
+        const changePriceFormat = (data) => {
+            return data.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        }
+
+
+        // 상태변경
+        const editStatus = () => {
+
+        }
+
+
+        // 모달창 닫기
+        const modalClose = (value) => {
+            state.isModalShow = value;
+        }
+
+        onMounted(() => {
+            loadData();
+        })
+
         return {
             state,
-            openModal
+            modalClose,
+            changePage,
+            search,
+            checkAllEvnet,
+            checkEvnet,
+            changeDateFormat,
+            changePriceFormat,
+            editStatus
         }
     }
 }
