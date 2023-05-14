a96355029344bdc6d7dff5374a3b8ae436b0e432
sunyoungAn <120145637+sunyoungAn@users.noreply.github.com>
2023-05-14 23:39:26 +0900

    문의관리리스트 구현
    문의관리리스트 구현

--- src/views/AdminInquiryListView.vue
+++ src/views/AdminInquiryListView.vue
@@ -3,170 +3,85 @@
         <admin-sub-menu></admin-sub-menu>
         <div class="admin_content">
             <h1 class="mb-4">| 문의관리</h1>
+
             <!-- 검색 영역 -->
             <div class="mb-5">
                 <h2 class="mb-4">문의 검색</h2>
                 <div class="px-5">
                     <div class="row mb-3">
                         <div class="col-2 py-2">
-                            회원번호
-                            <!-- <p class="text-center">번호</p> -->
+                            문의번호
                         </div>
                         <div class="col-4">
-                            <input type="text" class="form-control" placeholder="회원번호">
+                            <input type="text" class="form-control" v-model="state.searchId" placeholder="문의번호">
                         </div>
                         <div class="col-2 py-2">
                             문의유형
                         </div>
                         <div class="col-4">
-                            <select class="form-select brand_select" id="inputGroupSelect01">
-                                <option selected>Choose...</option>
-                                <option value="1">구매관련</option>
-                                <option value="2">판매관련</option>
-                                <option value="3">기타</option>
+                            <select class="form-select" v-model="state.searchCategory">
+                                <option v-for="status in state.categoryList" :key="status" :value="status.id">{{ status.value }}</option>
                             </select>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-2 py-2">
-                            문의제목
+                            회원번호
                         </div>
                         <div class="col-4">
-                            <input type="text" class="form-control" placeholder="문의제목">
+                            <input type="text" class="form-control" v-model="state.searchMemberNumber" placeholder="회원번호">
                         </div>
                         <div class="col-2 py-2">
                             문의처리상태
                         </div>
                         <div class="col-4">
-                            <select class="form-select brand_select" id="inputGroupSelect01">
-                                <option selected>Choose...</option>
-                                <option value="1">처리중</option>
-                                <option value="2">처리완료</option>
+                            <select class="form-select" v-model="state.searchInquiryStatus">
+                                <option v-for="status in state.inquiryStatusList" :key="status" :value="status.id">{{ status.value }}</option>
                             </select>
                         </div>
                     </div>
                     
                 </div>
                 <div class="d-flex justify-content-center mt-4">
-                    <button class="btn btn-success btn-lg">검색</button>
+                    <button class="btn btn-success btn-lg" @click="search()">검색</button>
                 </div>
             </div>
 
             <!-- 검색 결과-->
             <div>
-                <table class="table">
+                <table class="table align-middle">
                     <thead>
-                        <tr>
-                            <th scope="col"></th>
-                            <th scope="col">문의번호</th>
-                            <th scope="col">문의유형</th>
+                        <tr class="text-center">
+                            <th scope="col" width="50px">
+                                <input class="form-check-input" type="checkbox" :checked="state.checkAll" @change="checkAllEvnet()"> 
+                            </th>
+                            <th scope="col" width="100px">문의번호</th>
+                            <th scope="col" width="100px">문의유형</th>
                             <th scope="col">문의제목</th>
-                            <th scope="col">문의작성자</th>
-                            <th scope="col">문의처리상태</th>
-                            <th scope="col">문의등록일</th>
-                            <th scope="col">문의답변일</th>
+                            <th scope="col" width="120px">문의작성자</th>
+                            <th scope="col" width="120px">문의처리상태</th>
+                            <th scope="col" width="120px">문의등록일</th>
+                            <th scope="col" width="120px">문의답변일</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>구매관련해서 문의드립니다</td>
-                            <td>140</td>
-                            <td>처리완료</td>
-                            <td>2023/04/05</td>
-                            <td>2023/04/07</td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>배송은언제쯤되나요</td>
-                            <td>140</td>
-                            <td>처리중</td>
-                            <td>2023/04/05</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>배송은언제쯤되나요</td>
-                            <td>140</td>
-                            <td>처리중</td>
-                            <td>2023/04/05</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>배송은언제쯤되나요</td>
-                            <td>140</td>
-                            <td>처리중</td>
-                            <td>2023/04/05</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>배송은언제쯤되나요</td>
-                            <td>140</td>
-                            <td>처리중</td>
-                            <td>2023/04/05</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>배송은언제쯤되나요</td>
-                            <td>140</td>
-                            <td>처리중</td>
-                            <td>2023/04/05</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>배송은언제쯤되나요</td>
-                            <td>140</td>
-                            <td>처리중</td>
-                            <td>2023/04/05</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>배송은언제쯤되나요</td>
-                            <td>140</td>
-                            <td>처리중</td>
-                            <td>2023/04/05</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>배송은언제쯤되나요</td>
-                            <td>140</td>
-                            <td>처리중</td>
-                            <td>2023/04/05</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox" value=""></td>
-                            <td>1</td>
-                            <td>구매관련</td>
-                            <td>배송은언제쯤되나요</td>
-                            <td>140</td>
-                            <td>처리중</td>
-                            <td>2023/04/05</td>
-                            <td></td>
+                        <tr class="text-center" v-for="data of state.rows" :key="data">
+                            <td>
+                                <input class="form-check-input" type="checkbox" :value="data.id" v-model="state.checkList" @change="checkEvnet()">
+                            </td>
+                            <td>{{ data.id }}</td>
+                            <td>{{ data.category }}</td>
+                            <td @click="moveReply(data.id)" style="cursor: pointer;">{{ data.title }}</td>
+                            <td>
+                                <el-popover placement="top-start" :title=data.name :width="300" trigger="hover" :content=data.email>
+                                    <template #reference>
+                                        {{ data.memberNumber }}
+                                    </template> 
+                                </el-popover>
+                            </td>
+                            <td>{{ data.inquiryStatusName }}</td>
+                            <td>{{ changeDateFormat(data.inquiryRegistDate) }}</td>
+                            <td>{{ changeDateFormat(data.answerRegistDate) }}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -174,32 +89,22 @@
             
             <!-- 페이지 네이션-->
             <nav class="d-flex justify-content-center mt-4">
-                <ul class="pagination">
-                    <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                    </li>
-                </ul>
+                <el-pagination background layout="prev, pager, next" @current-change="changePage" :total="state.total" />
             </nav>
 
-            <!-- <div class="d-flex justify-content-center mt-3">
-                <button class="btn btn-success btn-lg d-flex justify-content-start">회원삭제</button>
-            </div> -->
+            <div class="d-flex justify-content-center mt-5">
+                <button class="btn btn-success btn-lg d-flex justify-content-start" @click="deleteInquiry()">문의삭제</button>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script>
 import AdminSubMenu from '@/components/AdminSubMenu.vue'
+import { onMounted, reactive } from 'vue'; 
+import axios from 'axios';
+import { useRouter } from 'vue-router';
 
 export default {
     name: 'SubMenu',
@@ -207,9 +112,189 @@ export default {
         AdminSubMenu
     },
     setup () {
-        
 
-        return {}
+        // 페이지 이동 라우트
+        const router = useRouter();
+        
+        // 상태변수
+        const state = reactive({
+            rows : [],
+            total : 0,
+            page : 0,
+            isSearch : false,
+            checkAll : false,
+            checkList : [],
+            searchId : '', // 검색조건
+            searchCategory : 0,
+            searchMemberNumber : '', // 검색조건
+            searchInquiryStatus : 0, // 검색조건
+            categoryList : [
+                { id : 0, value : '문의유형'},
+                { id : 1, value : '구매관련'},
+                { id : 2, value : '판매관련'},
+                { id : 3, value : '기타'},
+            ],
+            inquiryStatusList : [
+                { id : 0, value : '문의처리상태'},
+                { id : 1, value : '처리중'},
+                { id : 2, value : '처리완료'},
+            ],
+        })
+
+        // 문의관리리스트페이지에 띄울 정보 모두 가져오기
+        const loadData = () => {
+            axios.get(`/api/admin/inquiry/getall?page=${state.page}`).then((res)=>{
+                console.log(res.data);
+                state.rows = res.data.inquiryList;
+                state.total = res.data.total;
+                state.checkAll = false;
+                state.checkList = [];
+                // 검색조건 초기화
+                state.searchId = '';
+                state.searchCategory = 0;
+                state.searchMemberNumber = '';
+                state.searchInquiryStatus = 0;
+            }).catch(()=>{
+            })
+        }
+
+        // 페이징기능
+        const changePage = (page) => {
+            console.log(page);
+            state.page = page - 1; // 상태변수값 변경
+            if(state.isSearch === false) {
+                loadData(); // 데이터가져오기
+            } else {
+                search();
+            }
+        }
+
+        // 검색하기
+        const search = () => {
+
+            if( isNaN(state.searchId)) {
+                window.alert("문의번호는 숫자만 입력해주세요.");
+                state.searchId = '';
+                return false;
+            }
+
+            if( isNaN(state.searchMemberNumber)) {
+                window.alert("회원번호는 숫자만 입력해주세요.");
+                state.searchMemberNumber = '';
+                return false;
+            }
+
+            // 검색플래그 설정
+            if(state.searchId === '' && state.searchCategory === '' && 
+                state.searchMemberNumber === '' && state.searchInquiryStatus === 0) {
+                state.isSearch = false;
+            } else {
+                state.isSearch = true;
+            }
+
+            axios.get(`/api/admin/inquiry/search?page=${state.page}&id=${state.searchId}&category=${state.searchCategory}&memberNumber=${state.searchMemberNumber}&inquiryStatus=${state.searchInquiryStatus}`).then((res)=>{
+                console.log(res.data);
+                state.rows = res.data.inquiryList;
+                state.total = res.data.total;
+                state.checkAll = false;
+                state.checkList = [];
+            }).catch(()=>{
+            })
+
+        }
+
+        // 전체선택 처리
+        const checkAllEvnet = () => {
+            if(state.checkAll === false) {
+                for(let idx in state.rows) {
+
+                    // 이미 선택되어 있는 경우에는 중복선택 제외하기
+                    if(state.checkList.includes(state.rows[idx].id)) {
+                        continue;
+                    }
+                    state.checkList.push(state.rows[idx].id);
+                }
+                state.checkAll = true;
+            } else {
+                state.checkList = [];
+                state.checkAll = false;
+            }
+        }
+
+        // 체크박스 개별 처리
+        const checkEvnet = () => {
+            if(state.checkList.length === state.rows.length) {
+                state.checkAll = true;
+            } else {
+                state.checkAll = false;
+            }
+        }
+
+        // 날짜형식변환 yyyy/mm/dd
+        const changeDateFormat = (data) => {
+
+            if(data === null) {
+                return "-";
+            }
+
+            let date = new Date(data);
+            let year = date.getFullYear();
+            let month = ("0" + (1 + date.getMonth())).slice(-2);
+            let day = ("0" + date.getDate()).slice(-2);
+
+            return year + "/" + month + "/" + day;
+        }
+
+        // 문의삭제
+        const deleteInquiry = () => {
+
+            if(state.checkList.length === 0) {
+                window.alert("대상을 1개 이상 선택해주세요.");
+                return false;
+            }
+
+            if(confirm("선택한 문의를 삭제하겠습니까?")) {
+                // 처리완료데이터는 삭제 안되게 사전 체크
+                for(let idx in state.rows) {
+                    if(state.checkList.includes(state.rows[idx].id)) {
+                        if(state.rows[idx].inquiryStatus === 2) {
+                            window.alert("문의번호" + state.rows[idx].id +"번은 처리완료된 번호로 삭제할 수 없습니다.");
+                            return false;
+                        }
+                    }
+                }
+
+                //체크에 문제가 없으면 삭제처리 실행
+                axios.delete(`/api/admin/inquiry/delete/${state.checkList}`).then((res)=>{
+                    console.log(res.data);
+                    window.alert("삭제처리가 정상적으로 처리되었습니다.");
+                    loadData();
+                }).catch(()=>{
+                    window.alert("삭제처리 중 오류가 발생하였습니다.");
+                })
+            }
+
+        }
+
+        // 문의답변등록페이지로 이동
+        const moveReply = (id) => {
+            router.push({path:'/admin/inquiry/reply', query:{no : id}});
+        }
+
+        onMounted(() => {
+            loadData();
+        })
+
+        return {
+            state,
+            checkAllEvnet,
+            checkEvnet,
+            changeDateFormat,
+            changePage,
+            search,
+            deleteInquiry,
+            moveReply
+        }
     }
 }
 </script>
